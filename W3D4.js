64425a0e2316d50ced05b80ae503a2477be79586
skwon8 <saengsukwon@gmail.com>
2022-02-10 11:29:28 -0500

    W3D4 in python

---
+++ W3D4.js
@@ -0,0 +1,67 @@
+// selectionSort(arr) -  a slightly better sort!
+// return the array sorted in-place - don't create a new one
+
+// this sort relies on finding the minimum item in a subsection of the array,
+// so try writing another function:
+// minIndex(arr, start, end) - returns the index of the smallest item
+// found between indexes start and end (inclusive)
+
+function minIndex(arr, start, end) {
+    let minIdx = start;
+    for (var i = start; i <= end; i++) {
+        if (arr[i] < arr[minIdx]) {
+            minIdx = i;
+        }
+    }
+    return minIdx;
+}
+
+var test_array = [9, 0, 4, 6, 4, 1, 2, 7];
+console.log(minIndex(test_array, 0, 7)); // should return 1 - 0 is at the first index
+console.log(minIndex(test_array, 3, 6)); // should return 5 - 1 is at the fourth index
+console.log(minIndex(test_array, 1, 4)); // should return 1 - 0 is still at the first index
+console.log(minIndex(test_array, 2, 4)); // should return 2 - 4 is at the second
+// index, even though there's another 4 in the range we can settle for the first one
+
+var test_array2 = [9, 0, 4, 6, 4, 1, 2, -7, 8];
+console.log(minIndex(test_array2, 0, 7)); // should return 7
+
+// once you get that done, you can use it to implement selectionSort
+
+function selectionSort(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        var minIdx = minIndex(arr, i, arr.length - 1);
+        var temp = arr[i];
+        arr[i] = arr[minIdx];
+        arr[minIdx] = temp;
+    }
+}
+
+selectionSort(test_array2);
+console.log(test_array2); // should return [-7, 0, 1, 2, 4, 4, 6, 8, 9]
+
+
+// or we can combine those 2 functions to one as code down below. 
+
+var test_array2 = [9, 0, 4, 6, 4, 1, 2, -7, 8];
+console.log(minIndex(test_array2, 0, 7));
+
+function selectionSort(arr) {
+    for (var i = 0; i < arr.length; i++) {
+
+        let minIdx = i;
+
+        for (var j = i; j < arr.length; j++) {
+            if (arr[j] < arr[minIdx]) {
+                minIdx = j;
+            }
+        }
+
+        var temp = arr[i];
+        arr[i] = arr[minIdx];
+        arr[minIdx] = temp;
+    }
+}
+
+selectionSort(test_array2);
+console.log(test_array2);
